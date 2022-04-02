f8f6f7b602db092dd4c1de5acfea1dbe7dd12817
geuuuu <83645225+geuuuu@users.noreply.github.com>
2022-04-02 19:00:25 +0300

    Add files via upload

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 10f;
+    public float bulletDamage = 5f;
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * bulletSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float fireRate = 0.2f;
+    public Transform firingPoint;
+    public GameObject bulletPrefab;
+
+    float timeUntilFire;
+    PlayerMovement pm;
+
+    private void Start()
+    {
+        pm = gameObject.GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)&& timeUntilFire < timeUntilFire.time)
+        {
+            Shoot();
+            timeUntilFire = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        float angle = pm.isFacingRight ? 0f : 180f;
+        Instantiate(bulletPrefab, firingPoint.position, Quaternion.Euler(new Vector(0f, 0f, angle)));
+    }
+}

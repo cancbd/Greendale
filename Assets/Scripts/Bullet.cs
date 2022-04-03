a66109d6327515339f36203d1c68edd0026d53f4
geuuuu <emirhn.ysl@gmail.com>
2022-04-03 14:01:52 +0300

    change

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 10f;
+    public float bulletDamage = 5f;
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.right * bulletSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Rigidbody2D rgb;
+    Vector3 velocity;
+
+    float speedAmount = 3f;
+    float jumpAmount = 5f;
+
+    public bool isFacingRight = true;
+
+    void Start()
+    {
+        rgb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += velocity * speedAmount * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && Mathf.Approximately(rgb.velocity.y, 0)) {
+            rgb.AddForce(Vector3.up * jumpAmount, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == -1) {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            isFacingRight = false ;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            isFacingRight = true;
+        }
+    }
+
+    
+}
